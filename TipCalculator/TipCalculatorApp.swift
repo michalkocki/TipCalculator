8a47734c08f2f8c5a22f5ac488cb14c9f5c4a11f
michalkocki <michalkocki@gmail.com>
2021-12-15 20:33:06 +0100

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Michał Kocki on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
